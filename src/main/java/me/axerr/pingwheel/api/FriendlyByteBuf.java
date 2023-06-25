c0abc4b9d6d7ce760bfc50246ae6e4d80d0b9e99
Sand Residence <sandresidence@ultramail.pl>
2023-06-25 18:12:14 +0200

    Remove comments

--- src/main/java/me/axerr/pingwheel/api/FriendlyByteBuf.java
+++ src/main/java/me/axerr/pingwheel/api/FriendlyByteBuf.java
@@ -17,49 +17,34 @@ public class FriendlyByteBuf {
     }
 
     public String readString(int max) {
-        // Read declared length
         int length = readVarInt();
 
-        // Check if declared length is longer than maximum allowed
         if (length > max * 3)
             return null;
 
-        // Check if declared length is less than zero
         if (length < 0)
             return null;
 
-        // Read the string
         String string = source.toString(source.readerIndex(), length, StandardCharsets.UTF_8);
-
-        // Update reader index, so we will not read the same data again
         source.readerIndex(source.readerIndex() + length);
-
-        // Check if received string is longer than maximum allowed
         if (string.length() > max)
             return null;
 
         return string;
     }
 
-    // Used to read ping coords
     public double readDouble() {
         return source.readDouble();
     }
-
-    // Used to check if ping has entity UUID attached
     public boolean readBoolean() {
         return source.readBoolean();
     }
-
-    // Used to read entity uuid
     public UUID readUUID() {
         return new UUID(
                 source.readLong(),
                 source.readLong()
         );
     }
-
-    // Used to read string
     public int readVarInt() {
         int i = 0;
         int j = 0;
