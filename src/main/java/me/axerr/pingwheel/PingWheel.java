f83714bd555ab1025269665173773f6edc3d7bfe
Sand Residence <sandresidence@ultramail.pl>
2023-06-25 18:10:23 +0200

    Move payload listener to separate class

--- src/main/java/me/axerr/pingwheel/PingWheel.java
+++ src/main/java/me/axerr/pingwheel/PingWheel.java
@@ -1,27 +1,32 @@
 package me.axerr.pingwheel;
 
-import io.netty.buffer.Unpooled;
-import me.axerr.pingwheel.api.FriendlyByteBuf;
-import me.axerr.pingwheel.ratelimit.PlayerData;
-import me.axerr.pingwheel.ratelimit.RateLimit;
-import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.Messenger;
-import org.bukkit.plugin.messaging.PluginMessageListener;
 
-public final class PingWheel extends JavaPlugin implements PluginMessageListener {
+public final class PingWheel extends JavaPlugin {
+    @Getter
+    private static PingWheel plugin;
+
     @Override
     public void onEnable() {
+        plugin = this;
+
         if (!isPaper())
             return;
 
-        Config.loadConfig(this);
+        Config.loadConfig(plugin);
 
         Messenger messenger = getServer().getMessenger();
-        messenger.registerIncomingPluginChannel(this, "ping-wheel-c2s:ping-location", this);
+        messenger.registerIncomingPluginChannel(this, "ping-wheel-c2s:ping-location", new PingListener());
         messenger.registerOutgoingPluginChannel(this, "ping-wheel-s2c:ping-location");
+    }
+
+    @Override
+    public void onDisable() {
+        Messenger messenger = getServer().getMessenger();
+        messenger.unregisterIncomingPluginChannel(this, "ping-wheel-c2s:ping-location");
+        messenger.unregisterOutgoingPluginChannel(this, "ping-wheel-s2c:ping-location");
     }
 
     private boolean isPaper() {
@@ -30,7 +35,7 @@ public final class PingWheel extends JavaPlugin implements PluginMessageListener
         getLogger().severe("=====");
         getLogger().severe(" PingWheelBukkit requires PaperMC.io as your server software.");
         getLogger().severe("=====");
-        getServer().getPluginManager().disablePlugin(this);
+        getServer().getPluginManager().disablePlugin(plugin);
         return false;
     }
 
@@ -41,79 +46,5 @@ public final class PingWheel extends JavaPlugin implements PluginMessageListener
         } catch (ClassNotFoundException e) {
             return false;
         }
-    }
-
-    @Override
-    public void onDisable() {
-        Messenger messenger = getServer().getMessenger();
-        messenger.unregisterIncomingPluginChannel(this, "ping-wheel-c2s:ping-location");
-        messenger.unregisterOutgoingPluginChannel(this, "ping-wheel-s2c:ping-location");
-    }
-
-    @Override
-    public void onPluginMessageReceived(String channel, Player player, byte[] message) {
-        // Cast data to object
-        FriendlyByteBuf data = new FriendlyByteBuf(Unpooled.copiedBuffer(message));
-        Ping ping = new Ping(
-                data.readString(),
-                data.readDouble(),
-                data.readDouble(),
-                data.readDouble(),
-                data.readBoolean(),
-                null
-        );
-        if (ping.isEntity())
-            ping.setEntityUUID(data.readUUID());
-
-        // Validate entity ping
-        if (ping.isEntity() && (!Config.ENTITY_PING_ENABLED || (Config.ENTITY_PING_PERMISSION_ENABLED && !player.hasPermission(Config.ENTITY_PING_PERMISSION)))) {
-            if (!Config.ENTITY_PING_NO_PERMISSION_MESSAGE.isEmpty())
-                player.sendMessage(MiniMessage.miniMessage().deserialize(Config.ENTITY_PING_NO_PERMISSION_MESSAGE));
-            return;
-        }
-
-        // Validate location ping
-        if (!ping.isEntity() && ((!Config.LOCATION_PING_ENABLED) || (Config.LOCATION_PING_PERMISSION_ENABLED && !player.hasPermission(Config.LOCATION_PING_PERMISSION)))) {
-            if (!Config.LOCATION_PING_NO_PERMISSION_MESSAGE.isEmpty())
-                player.sendMessage(MiniMessage.miniMessage().deserialize(Config.LOCATION_PING_NO_PERMISSION_MESSAGE));
-            return;
-        }
-
-        // Validate limits data
-        if (Config.LIMIT_X_ENABLED && (ping.getX() > Config.LIMIT_X_MAX || ping.getX() < Config.LIMIT_X_MIN))
-            return;
-        if (Config.LIMIT_Y_ENABLED && (ping.getY() > Config.LIMIT_Y_MAX || ping.getY() < Config.LIMIT_Y_MIN))
-            return;
-        if (Config.LIMIT_Z_ENABLED && (ping.getZ() > Config.LIMIT_Z_MAX || ping.getZ() < Config.LIMIT_Z_MIN))
-            return;
-
-        // Check rate limit
-        PlayerData playerData = RateLimit.getData(player.getUniqueId());
-        if (!playerData.canPing(player)) {
-            if (!Config.RATE_LIMIT_MESSAGE.isEmpty())
-                player.sendMessage(MiniMessage.miniMessage().deserialize(Config.RATE_LIMIT_MESSAGE));
-            return;
-        }
-        playerData.ping();
-
-        // Broadcast ping
-        for (Player onlinePlayer : Bukkit.getOnlinePlayers())
-            if (onlinePlayer.getWorld() == player.getWorld())
-                onlinePlayer.sendPluginMessage(
-                        this,
-                        "ping-wheel-s2c:ping-location",
-                        message
-                );
-
-        // Logging
-        if (Config.LOGGING_ENABLED)
-            getLogger().info(Config.LOGGING_MESSAGE
-                    .replace("%player%", player.getName())
-                    .replace("%entity%", String.valueOf(ping.isEntity()))
-                    .replace("%x%", String.valueOf(ping.getX()))
-                    .replace("%y%", String.valueOf(ping.getY()))
-                    .replace("%z%", String.valueOf(ping.getZ()))
-                    .replace("%channel%", ping.getChannel())
-            );
     }
 }
